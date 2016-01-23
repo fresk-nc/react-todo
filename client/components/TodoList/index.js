a75aa3475eb5730626ce69f41a32cf8ee2c1fd40
fresk <fresk@yandex-team.ru>
2016-01-24 00:35:02 +0300

    add dummy for todo-list

--- client/components/TodoList/index.js
+++ client/components/TodoList/index.js
@@ -7,20 +7,37 @@ export default React.createClass({
     displayName: 'TodoList',
 
     render: function() {
-        var items = this.props.items;
-        var nodes = [];
+        return (
+            <div className="todo-list">
+                {this._renderContent()}
+            </div>
+        );
+    },
+
+    _renderContent() {
+        let items = this.props.items;
+        let nodes = [];
 
         for (var key in items) {
             nodes.push(<ComponentTodoItem key={key} todo={items[key]} />);
         }
 
-        return (
-            <div className="todo-list">
-                <ReactCSSTransitionGroup transitionName="todo-list" transitionEnterTimeout={300} transitionLeaveTimeout={300}>
+        if (nodes.length > 0) {
+            return (
+                <ReactCSSTransitionGroup
+                    transitionName="todo-list"
+                    transitionEnterTimeout={300}
+                    transitionLeaveTimeout={300}>
                     {nodes}
                 </ReactCSSTransitionGroup>
-            </div>
-        );
+            );
+        } else {
+            return (
+                <div className="todo-list__empty">
+                    It's the nice time to do something
+                </div>
+            );
+        }
     }
 
 });
