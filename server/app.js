58b2cf2cf674e6f5f2e1b36c10f134928ddab4e0
fresk <fresk@yandex-team.ru>
2016-01-30 12:55:41 +0300

    move items to json

--- server/app.js
+++ server/app.js
@@ -1,36 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var items = require('./items.json');
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use('/api', (req, res) => {
     if (req.body.method === 'items') {
-        res.send({
-            '1': {
-                id: 1,
-                title: 'buy eggs',
-                complete: false,
-                isNew: false
-            },
-            '2': {
-                id: 2,
-                title: 'buy milk',
-                complete: false,
-                isNew: false
-            },
-            '3': {
-                id: 3,
-                title: 'buy bread',
-                complete: true,
-                isNew: false
-            }
-        });
-    } else {
-        res.send({
-            status: 'ok'
-        });
+        res.send(items);
     }
+
+    res.send({
+        status: 'ok'
+    });
 });
 
 app.listen(8000, 'localhost', (err) => {
