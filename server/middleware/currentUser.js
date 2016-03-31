287beccf45b91fe837727dac3d69fcfd0b2bc269
fresk <fresk@yandex-team.ru>
2016-03-31 22:41:03 +0300

    set a cookie for a year

--- server/middleware/currentUser.js
+++ server/middleware/currentUser.js
@@ -2,12 +2,15 @@
 
 const uuid = require('node-uuid');
 
+const YEAR_IN_MS = 31536000000;
+
 module.exports = function(req, res, next) {
     let uid = req.cookies.uid;
 
     if (!uid) {
         uid = uuid.v1();
         res.cookie('uid', uid, {
+            expires: new Date(Date.now() + YEAR_IN_MS),
             httpOnly: true
         });
         req.cookies.uid = uid;
